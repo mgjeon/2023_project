48e6c886409cd90edee57c454eff9f662a05fc5f
mgjeon <mgjeon@khu.ac.kr>
2023-10-17 10:29:55 +0900

    NF2 cnn

--- NF2_notebooks/cnn/tool/evaluate.py
+++ NF2_notebooks/cnn/tool/evaluate.py
@@ -17,9 +17,11 @@ def metric(b, B):
     result['c_vec'] = np.sum(dot_product(B, b)) / np.sqrt(np.sum(vector_norm(B)**2) * np.sum(vector_norm(b)**2))
     
     M = np.prod(B.shape[:-1])
+    eps = 1e-7
     nu = dot_product(B, b)
     de = vector_norm(B) * vector_norm(b)
     result['c_cs'] = (1 / M) * np.sum(np.divide(nu, de, where=de!=0.))
+    # result['c_cs'] = (1 / M) * np.sum(nu/(de + eps))
 
     E_n = np.sum(vector_norm(b - B)) / np.sum(vector_norm(B))
     result["E_n'"] = 1 - E_n
@@ -27,6 +29,7 @@ def metric(b, B):
     nu = vector_norm(b - B)
     de = vector_norm(B)
     E_m = (1 / M) * np.sum(np.divide(nu, de, where=de!=0.))
+    # E_m = (1 / M) * np.sum((nu/(de + eps)))
     result["E_m'"] = 1 - E_m
 
     result['eps'] = np.sum(vector_norm(b)**2) / np.sum(vector_norm(B)**2)
